f82a465d020ccc10a2c040b4786d2706705359ad
OlacodesLA <96698181+OlacodesLA@users.noreply.github.com>
2022-06-26 13:55:47 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { getPosts } from '../utils/mdx-utils';
+import Main from '../components/Main';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -7,45 +8,48 @@ import Layout, { GradientBackground } from '../components/Layout';
 import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import Construction from '../assets/SCHOOL.png';
+import Cone from '../assets/cone.png';
+import Image from 'next/image';
 
 export default function Index({ posts, globalData }) {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
-      <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <Image
+        src={Construction}
+        alt="Construction"
+        className="rounded border-2 border-violet-400 mx-2"
+        layout="intrinsic"
+        data-aos="fade-right"
+        data-aos-duration="3000"
+        width={755}
+        height={354}
+      />
+      <div className="pt-6 text-center">
+        <Image
+          src={Cone}
+          alt="Cone"
+          className="rounded border-2 border-violet-400 pt-10"
+          layout="intrinsic"
+          data-aos="zoom-in"
+          data-aos-duration="2000"
+          width={60}
+          height={73.8}
+        />
+        <p className="text-2xl  text-transparent font-bold bg-clip-text bg-gradient-to-r from-indigo-900 to-indigo-600 dark:bg-gradient-to-r dark:from-blue-200 dark:to-blue-100">
+          This Site Is Under Construction!
+        </p>
+      </div>
+
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground
         variant="large"
