e3ed8b03688600e5a9559759d5df968d45b60097
OlacodesLA <96698181+OlacodesLA@users.noreply.github.com>
2022-06-26 13:54:49 +0100

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
 import Link from 'next/link';
+import Logo from '../assets/logo.svg';
+import Image from 'next/image';
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-      <p className="text-2xl dark:text-white text-center">
+    <header className="pt-6 text-center">
+      <Image
+        src={Logo}
+        alt="Sharon Springs Logo"
+        layout="fixed"
+        width={120}
+        height={120}
+      />
+      <p className="text-2xl dark:text-white text-indigo-900 font-bold pb-2 text-center">
         <Link href="/">
           <a>{name}</a>
         </Link>
